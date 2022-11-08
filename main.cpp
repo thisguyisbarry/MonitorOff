5c08d8a1957ed1028c01be013d773b17451af83e
thisguyisbarry <thisguyisbarry@users.noreply.github.com>
2022-11-08 17:24:44 +0000

    Abandoning the idea of using c in c++

--- main.cpp
+++ main.cpp
@@ -1,14 +1,8 @@
 // This programme will continuously read for input, 
 // and perform actions based on that input (or do nothing!)
 
-int main(){
-    extern "C" {
-        while (1){
-            if ( _kbhit() ){
-                key_code = _getch();
-                
-            }
-        else 
-            continue;
-    }
+#include <iostream>
+
+int main() {
+    
 }
