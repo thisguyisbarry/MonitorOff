e15faefc8fe637da3ac52dd30f727a8dd855f1e4
thisguyisbarry <thisguyisbarry@users.noreply.github.com>
2022-11-12 10:33:32 +0000

    Working state. Not perfect but cool.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 This application is an executable that you can pin to your task bar, or place where ever you so wish. 
 Upon launching the application any displays you have connected will be switched off, and the vast majority of input will be ignored to avoid switching on your monitor. 
-The only inputs that will be accepted will be F12 to turn back on your monitor, or any volume settings. 
+Any input that would wake up your screen will not do so. The scroll lock button will be used to wake your monitor back up.
+
+This is useful for example if you wish to keep your monitors in standby while playing some sound from your computer, but still wish to be able to control media volume. 
--- main.cpp
+++ main.cpp
@@ -7,16 +7,23 @@
 #include <Windows.h>
 
 int main() {
-    HWND handle = GetForegroundWindow();
+    HWND handle = GetForegroundWindow(); // Just initialising the handler
+    
+    std::cout << "Start\n";
+    SendMessage(handle, WM_SYSCOMMAND, SC_MONITORPOWER, 2); 
+    
     while(1) {
-        if (GetAsyncKeyState('A') & 0x8000) {
-            std::cout << "'A' key is pressed\n";
-            SendMessage(handle, WM_SYSCOMMAND, SC_MONITORPOWER, 2); 
+        if (GetAsyncKeyState(VK_SCROLL) & 0x0001) {
+            std::cout << "Scroll lock key is pressed\n";
+            break; 
         } else {
-            std::cout << "'A' key is not pressed\n";
+            SendMessage(handle, WM_SYSCOMMAND, SC_MONITORPOWER, 2); 
+            std::cout << "Scroll lock key is not pressed\n";
         }
         Sleep(250);
     }
+
+    std::cout << "End\n";
 
     return 0;
 }
